6187d56fcc0cf3157b518116a12c03994bedbe55
c0513 <57907072+c0513@users.noreply.github.com>
2026-09-25 19:19:49 +0300

    Use native CommerceML 2.10 namespace for 1C payments

--- server.js
+++ server.js
@@ -11254,7 +11254,7 @@ function oneCOrdersCommerceMl(orders) {
 
     return [
         '<?xml version="1.0" encoding="UTF-8"?>',
-        `<КоммерческаяИнформация xmlns="urn:1C.ru:commerceml_2" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xs:anyType" ВерсияСхемы="2.10" ДатаФормирования="${now}">`,
+        `<КоммерческаяИнформация xmlns="urn:1C.ru:commerceml_210" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ВерсияСхемы="2.10" ДатаФормирования="${now}">`,
         ...orders.map(
             order =>
                 oneCOrderXml(order)
